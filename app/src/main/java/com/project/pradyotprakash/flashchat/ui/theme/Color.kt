157b2899791ecf917b97db06ca95874d1c99fecc
akg807 <80644244+akg807@users.noreply.github.com>
2023-07-02 22:21:59 +0530

    final

---
+++ app/src/main/java/com/project/pradyotprakash/flashchat/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.project.pradyotprakash.flashchat.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFFCEAB93)
+//val White = Color(0xFFFFFFFF)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
